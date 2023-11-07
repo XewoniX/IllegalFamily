dd3783839ab52d37ccd2f580089d26cdcd40d054
XewoniX <xewonix36@gmail.com>
2023-11-07 06:15:41 +0100

    Pierwszy_chat_dziala

--- app/src/main/java/com/jakubsapplication/app/modules/chatviewcontainer/ui/ChatViewContainerActivity.kt
+++ app/src/main/java/com/jakubsapplication/app/modules/chatviewcontainer/ui/ChatViewContainerActivity.kt
@@ -6,6 +6,9 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
+import android.view.inputmethod.InputMethodManager
+import android.widget.ScrollView
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -26,108 +29,128 @@ import com.jakubsapplication.app.modules.votingview.ui.VotingViewActivity
 
 class ChatViewContainerActivity :
     BaseActivity<ActivityChatViewContainerBinding>(R.layout.activity_chat_view_container) {
-  private val viewModel: ChatViewContainerVM by viewModels<ChatViewContainerVM>()
-  data class Message(
-    val senderEmail: String,
-    val messageContent: String,
-    val timestamp: Long
-  )
-  private val messages = mutableListOf<Message>()
-  private lateinit var adapter: MessageAdapter
-  private lateinit var firestore: FirebaseFirestore
-  private var messageListener: ListenerRegistration? = null
-  override fun onInitialized(): Unit {
-    viewModel.navArguments = intent.extras?.getBundle("bundle")
-    binding.chatViewContainerVM = viewModel
+    private val viewModel: ChatViewContainerVM by viewModels<ChatViewContainerVM>()
 
-    window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN or
-            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+    data class Message(
+        val senderEmail: String,
+        val messageContent: String,
+        val timestamp: Long
+    )
 
-    val recyclerView: RecyclerView = findViewById(R.id.recyclerViewChat)
-    recyclerView.layoutManager = LinearLayoutManager(this)
-    adapter = MessageAdapter(messages)
-    recyclerView.adapter = adapter
+    private val messages = mutableListOf<Message>()
+    private lateinit var adapter: MessageAdapter
+    private lateinit var firestore: FirebaseFirestore
+    private var messageListener: ListenerRegistration? = null
+    override fun onInitialized(): Unit {
+        viewModel.navArguments = intent.extras?.getBundle("bundle")
+        binding.chatViewContainerVM = viewModel
 
-    firestore = FirebaseFirestore.getInstance()
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 
-    // Rozpocznij nasłuchiwanie na kolekcję "Messages" w Firebase Firestore
-    messageListener = firestore.collection("Messages")
-      .orderBy("timestamp")
-      .addSnapshotListener { snapshot, e ->
-        if (e != null) {
-          // Obsługa błędów
-          return@addSnapshotListener
-        }
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerViewChat)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = MessageAdapter(messages)
+        recyclerView.adapter = adapter
 
-        messages.clear()
-        if (snapshot != null) {
-          for (document in snapshot) {
-            val senderEmail = document.getString("senderEmail") ?: ""
-            val messageContent = document.getString("messageContent") ?: ""
-            val timestamp = document.getTimestamp("timestamp")?.toDate()?.time ?: 0
+        firestore = FirebaseFirestore.getInstance()
 
-            val message = Message(senderEmail, messageContent, timestamp)
-            messages.add(message)
+        // Rozpocznij nasłuchiwanie na kolekcję "Messages" w Firebase Firestore
+        messageListener = firestore.collection("Messages")
+            .orderBy("timestamp")
+            .addSnapshotListener { snapshot, e ->
+                if (e != null) {
+                    // Obsługa błędów
+                    return@addSnapshotListener
+                }
+
+                messages.clear()
+                if (snapshot != null) {
+                    for (document in snapshot) {
+                        val senderEmail = document.getString("senderEmail") ?: ""
+                        val messageContent = document.getString("messageContent") ?: ""
+                        val timestamp = document.getTimestamp("timestamp")?.toDate()?.time ?: 0
+
+                        val message = Message(senderEmail, messageContent, timestamp)
+                        messages.add(message)
+                    }
+                }
+
+                adapter.notifyDataSetChanged()
             }
-        }
+    }
 
-        adapter.notifyDataSetChanged()
-      }
-  }
-
-  override fun onDestroy() {
-    super.onDestroy()
-    // Zatrzymaj nasłuchiwanie, aby uniknąć wycieków pamięci
-    messageListener?.remove()
-  }
+    override fun onDestroy() {
+        super.onDestroy()
+        // Zatrzymaj nasłuchiwanie, aby uniknąć wycieków pamięci
+        messageListener?.remove()
+    }
 
     override fun setUpClicks(): Unit {
-      binding.SendMessage.setOnClickListener {
-        val user = Firebase.auth.currentUser
-        val email = user?.email
-        val chat_message = findViewById<TextInputEditText>(R.id.editTextChat)
-        val db = FirebaseFirestore.getInstance()
-        val messageData = hashMapOf(
-          "senderEmail" to email,
-          "messageContent" to chat_message.text.toString(),
-          "timestamp" to FieldValue.serverTimestamp()
-        )
+        val scrollView = findViewById<ScrollView>(R.id.ScrollChat)
 
-        db.collection("Messages")
-          .add(messageData)
-          .addOnSuccessListener { documentReference ->
-            // Wiadomość została dodana do Firestore
-          }
-          .addOnFailureListener { e ->
-            // Błąd podczas dodawania wiadomości
-          }
-        chat_message.text = null
-      }
-      binding.imageLineOne.setOnClickListener {
-        val destIntent = VotingViewActivity.getIntent(this, null)
-        startActivity(destIntent)
-      }
-      binding.imageUser.setOnClickListener {
-        val destIntent = ProfileSettingViewActivity.getIntent(this, null)
-        startActivity(destIntent)
-      }
-      binding.imageHome.setOnClickListener {
-        val destIntent = HomeViewActivity.getIntent(this, null)
-        startActivity(destIntent)
-      }
-      binding.imageMap.setOnClickListener {
-        val destIntent = MapViewActivity.getIntent(this, null)
-        startActivity(destIntent)
-      }
+        binding.SendMessage.setOnClickListener {
+            val user = Firebase.auth.currentUser
+            val email = user?.email
+            val chat_message = findViewById<TextInputEditText>(R.id.editTextChat)
+            val db = FirebaseFirestore.getInstance()
+            if (chat_message.text.toString().length >= 1) {
+                val messageData = hashMapOf(
+                    "senderEmail" to email,
+                    "messageContent" to chat_message.text.toString(),
+                    "timestamp" to FieldValue.serverTimestamp()
+                )
+
+                db.collection("Messages")
+                    .add(messageData)
+                    .addOnSuccessListener { documentReference ->
+                        // Wiadomość została dodana do Firestore
+                    }
+                    .addOnFailureListener { e ->
+                        // Błąd podczas dodawania wiadomości
+                    }
+                chat_message.text = null
+            } else {
+                Toast.makeText(
+                    getApplicationContext(),
+                    "Nie mozna wyslac pustej wiadomosci.", Toast.LENGTH_SHORT
+                ).show();
+            }
+            val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(binding.SendMessage.windowToken, 0)
+            val recyclerView = findViewById<RecyclerView>(R.id.recyclerViewChat)
+            val positionToScroll = adapter.itemCount - 1
+            recyclerView.scrollToPosition(positionToScroll)
+            scrollView.postDelayed({
+                scrollView.fullScroll(ScrollView.FOCUS_DOWN)
+            }, 500)
+
+        }
+        binding.imageLineOne.setOnClickListener {
+            val destIntent = VotingViewActivity.getIntent(this, null)
+            startActivity(destIntent)
+        }
+        binding.imageUser.setOnClickListener {
+            val destIntent = ProfileSettingViewActivity.getIntent(this, null)
+            startActivity(destIntent)
+        }
+        binding.imageHome.setOnClickListener {
+            val destIntent = HomeViewActivity.getIntent(this, null)
+            startActivity(destIntent)
+        }
+        binding.imageMap.setOnClickListener {
+            val destIntent = MapViewActivity.getIntent(this, null)
+            startActivity(destIntent)
+        }
     }
 
     companion object {
-      const val TAG: String = "CHAT_VIEW_CONTAINER_ACTIVITY"
-      fun getIntent(context: Context, bundle: Bundle?): Intent {
-        val destIntent = Intent(context, ChatViewContainerActivity::class.java)
-        destIntent.putExtra("bundle", bundle)
-        return destIntent
-      }
+        const val TAG: String = "CHAT_VIEW_CONTAINER_ACTIVITY"
+        fun getIntent(context: Context, bundle: Bundle?): Intent {
+            val destIntent = Intent(context, ChatViewContainerActivity::class.java)
+            destIntent.putExtra("bundle", bundle)
+            return destIntent
+        }
     }
-  }
+}
--- app/src/main/java/com/jakubsapplication/app/modules/chatviewcontainer/ui/RecyclerViewChat.kt
+++ app/src/main/java/com/jakubsapplication/app/modules/chatviewcontainer/ui/RecyclerViewChat.kt
@@ -1,6 +1,12 @@
+import android.R.attr.bottom
+import android.R.attr.left
+import android.R.attr.right
+import android.R.attr.top
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.Firebase
@@ -9,12 +15,15 @@ import com.google.firebase.firestore.FirebaseFirestore
 import com.jakubsapplication.app.R
 import com.jakubsapplication.app.modules.chatviewcontainer.ui.ChatViewContainerActivity
 
+
 data class Message(
     val senderEmail: String,
     val messageContent: String,
     val timestamp: Long
 )
 var pole = ""
+val drawableResId = R.drawable.rectangle_gradient_s_grey_400_e_cyan_a202_radius_20
+var VIEW_TYPE_EMAIL_EXAMPLE = 0
 class MessageAdapter(private val messages: MutableList<ChatViewContainerActivity.Message>) :
     RecyclerView.Adapter<MessageAdapter.MessageViewHolder>() {
 
@@ -23,20 +32,33 @@ class MessageAdapter(private val messages: MutableList<ChatViewContainerActivity
         val messageContentTextView: TextView = itemView.findViewById(R.id.messageContentTextView)
     }
 
+    override fun getItemViewType(position: Int): Int {
+        val user = Firebase.auth.currentUser
+        val email = user?.email
+        val message = messages[position]
+        if (email == message.senderEmail) {
+            VIEW_TYPE_EMAIL_EXAMPLE = 0
+            return VIEW_TYPE_EMAIL_EXAMPLE
+        } else {
+            VIEW_TYPE_EMAIL_EXAMPLE = 1
+            return VIEW_TYPE_EMAIL_EXAMPLE
+        }
+    }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageViewHolder {
-        val itemView = LayoutInflater.from(parent.context)
-            .inflate(R.layout.item_message_moja, parent, false)
+        val itemView: View
+        if (VIEW_TYPE_EMAIL_EXAMPLE == 1) {
+            itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_message_moja, parent, false)
+        } else {
+            itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_message_kogos, parent, false)
+        }
         return MessageViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: MessageViewHolder, position: Int) {
-
-
+        val message = messages[position]
         val db = FirebaseFirestore.getInstance()
         val kolekcjaRef = db.collection("QRAuth")
-        val user = Firebase.auth.currentUser
-        val email = user?.email
-        val zapytanie = kolekcjaRef.whereEqualTo("adres_email", email)
+        val zapytanie = kolekcjaRef.whereEqualTo("adres_email", message.senderEmail)
         zapytanie.get()
             .addOnSuccessListener { querySnapshot ->
                 for (document in querySnapshot.documents) {
@@ -68,9 +90,18 @@ class MessageAdapter(private val messages: MutableList<ChatViewContainerActivity
             .addOnFailureListener { e ->
                 println("Błąd podczas wykonywania zapytania: $e")
             }
-
-        val message = messages[position]
         holder.messageContentTextView.text = message.messageContent
+    /*    if(email == message.senderEmail)
+        {
+            holder.senderEmailTextView.visibility = View.GONE
+            holder.messageFrame.setBackgroundResource(drawableResId)
+            val params = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+            params.setMargins(0, 10, 100, 0,)
+            holder.messageFrame.setLayoutParams(params)
+        }*/
     }
     override fun getItemCount(): Int {
         return messages.size
