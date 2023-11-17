878a498550406d8548cc3a859e54869940ed8dd3
XewoniX <xewonix36@gmail.com>
2023-11-17 23:03:46 +0100

    mapa_dodana_nie_dziala

--- app/src/main/java/com/jakubsapplication/app/modules/mapview/ui/MapViewActivity.kt
+++ app/src/main/java/com/jakubsapplication/app/modules/mapview/ui/MapViewActivity.kt
@@ -2,10 +2,20 @@ package com.jakubsapplication.app.modules.mapview.ui
 
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationRequest
 import android.os.Bundle
+import android.os.Looper
 import android.view.View
 import android.view.WindowManager
 import androidx.activity.viewModels
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.jakubsapplication.app.Manifest
 import com.jakubsapplication.app.R
 import com.jakubsapplication.app.appcomponents.base.BaseActivity
 import com.jakubsapplication.app.databinding.ActivityMapViewBinding
@@ -27,6 +37,28 @@ class MapViewActivity : BaseActivity<ActivityMapViewBinding>(R.layout.activity_m
     window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN or
             View.SYSTEM_UI_FLAG_LAYOUT_STABLE
     window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+
+    val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+    mapFragment.getMapAsync { googleMap ->
+      // Włączanie funkcji śledzenia lokalizacji
+     // googleMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+     // googleMap.uiSettings.isZoomControlsEnabled = true // Włącz suwak zoomu
+     // googleMap.uiSettings.isCompassEnabled = true // Włącz kompas
+    //  googleMap.uiSettings.isMapToolbarEnabled = true // Włącz pasek narzędzi mapy
+
+
+
+      val specificLocation = LatLng(51.5074, -0.1278) // Londyn jako przykład
+
+      // Przesunięcie kamery do konkretnej lokalizacji (bez animacji)
+      //googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(specificLocation, 15f))
+
+      // Lub z animacją
+      googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(specificLocation, 15f))
+
+    }
+
   }
 
   override fun setUpClicks(): Unit {
